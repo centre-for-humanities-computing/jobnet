90b4feb2eaf1111fdbb4de81eea3f488eda8c9d9
kate <sk.borisova@gmail.com>
2022-06-10 10:33:41 +0200

    script for semantic graphs transformed from a notebook to a .py file

---
+++ src/application/semkern.py
@@ -0,0 +1,26 @@
+"""Pipeline for training word2vec model on job posts and plotting semantic graphs"""
+import sys
+
+sys.path.append("../../../semantisk-kernel")
+import pandas as pd
+from gensim.models import Word2Vec
+from semkern.model import train_model
+from semkern.graphing import plot
+
+
+df = pd.read_pickle("../../data/pkl/dataset.pkl")
+
+posts = df["lemmas"].to_list()
+
+model = train_model(posts)
+model.save("word2vec.model")
+model = Word2Vec.load("word2vec.model")
+
+seeds = ["professor"]
+
+k = 5
+m = 3
+
+fig = plot(seeds, k, m, model)
+fig.show()
+fig.write_image("../../figs/w2v/professor.pdf")
